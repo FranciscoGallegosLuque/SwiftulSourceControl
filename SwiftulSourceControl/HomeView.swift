ab65de310e4af9bb7a340a503766a836a8807609
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-03-18 14:57:12 -0300

    [Feature] Home

---
+++ SwiftulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftulSourceControl
+//
+//  Created by Francisco Manuel Gallegos Luque on 18/03/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
