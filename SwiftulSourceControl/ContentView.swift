f4c9fdba6e5b69bd9e3782f69113a30c4277c376
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-03-18 08:19:19 -0300

    [Feature] Rename and added comment

--- SwiftulSourceControl/ContentView.swift
+++ SwiftulSourceControl/ContentView.swift
@@ -5,6 +5,32 @@
 //  Created by Francisco Manuel Gallegos Luque on 17/03/2025.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ Clone= = Copying the repo locally
+ Commit = Save on our current branch
+ Stage = Prepare changes for a commit
+ Stash = Save changes for later
+ Push = Send local commits to remote repo
+ Pull = Fetch remote commits to local repo
+ 
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of the release
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
@@ -15,7 +41,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Swiftul Thinking!")
             
-            Button("Click me") {
+            Button("Click here") {
                 
             }
         }
